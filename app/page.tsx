8096a50aa4f369793ab97c3a4c96a99eef1feb8b
FabioDiCeglie <fabiod@backbase.com>
2024-04-03 23:05:28 +0200

    chore: start working on the design

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import { SettingsIcon } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className='bg-black h-screen overflow-y-auto'>
       {/* Header */}
       <header className='flex justify-between fixed top-0 text-white w-full p-5'>
-        <div className='rounded-full overflow-hidden'>
-        <Image
-          src='https://i.pinimg.com/564x/b4/3b/f3/b43bf303f475ddeb69751c67c1ae3856.jpg'
-          alt='Logo'
-          height={50}
-          width={50}
-          className='object-contain rounded-full'
-        />
+        <div className='rounded-full'>
+          <Image
+            src='https://i.pinimg.com/564x/b4/3b/f3/b43bf303f475ddeb69751c67c1ae3856.jpg'
+            alt='Logo'
+            height={40}
+            width={40}
+            className='object-contain rounded-full'
+          />
         </div>
 
         <SettingsIcon
@@ -25,6 +25,21 @@ export default function Home() {
       </header>
 
       {/* Form */}
+      <form action='flex flex-col bg-black'>
+        <div className='flex-1 bg-gradient-to-b from-purple-500 to-black'>
+          {/* Messages */}
+        </div>
+
+        <input type='file' hidden />
+        <button type='submit' hidden />
+
+        <div className='fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl'>
+          {/* Recorder */}
+
+          {/* Voice Synthesiser - output of the Assistance voice */}
+          <div></div>
+        </div>
+      </form>
     </main>
   );
 }
