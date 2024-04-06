9727e73e51aa3c47ab0b31fbd5bea981f4fe1c16
FabioDiCeglie <fabiod@backbase.com>
2024-04-06 21:50:19 +0200

    chore: change favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Siri 2.0",
-  description: "Siri prototype",
+  title: 'Siri 2.0',
+  description: 'Siri prototype',
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className='h-screen overflow-hidden'>{children}</body>
     </html>
   );
